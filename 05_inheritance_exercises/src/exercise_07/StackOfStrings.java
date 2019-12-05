6a206985e3d4d47d9d739cc6885eb769667ecce5
Cvqtko <cvqtkokirilov@gmail.com>
2019-12-05 17:25:37 +0200

    some more exercises

---
+++ 05_inheritance_exercises/src/exercise_07/StackOfStrings.java
@@ -0,0 +1,31 @@
+package exercise_07;
+
+import java.util.ArrayList;
+
+public class StackOfStrings {
+	private ArrayList<String> data;
+
+	StackOfStrings() {
+		this.data = new ArrayList<>();
+	}
+
+	public boolean isEmpty() {
+		return this.data.isEmpty();
+	}
+
+	public void push(String item) {
+		this.data.add(item);
+	}
+	
+	public String pop() {
+		return this.data.remove(getLastIndex());
+	}
+
+	public String peek() {
+		return this.data.get(getLastIndex());
+	}
+	
+	private int getLastIndex() {
+		return this.data.size()-1;
+	}
+}
