f88cea85edc04c72dfa9b3745ae170cd0f3bfe3f
Cvqtko <cvqtkokirilov@gmail.com>
2019-12-19 15:29:53 +0200

    Dummy tests

--- 19_unit_testing/src/test/java/rpg_lab/DummyTest.java
+++ 19_unit_testing/src/test/java/rpg_lab/DummyTest.java
@@ -9,9 +9,25 @@ public class DummyTest {
 	public void dummyShouldLoseHealthWhenAttacked() {
 
 		Dummy dummy = new Dummy(10, 10);
-		Axe axe = new Axe(50, 5);
-		axe.attack(dummy);
+		dummy.takeAttack(5);
 
-		Assert.assertTrue(axe.getDurabilityPoints() == 4);
+		Assert.assertTrue(dummy.getHealth() == 5);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void shouldThrowExceptionWhenAttackingDeadDummy() {
+		Dummy dummy = new Dummy(-5, 10);
+		dummy.takeAttack(5);
+
+		Assert.assertTrue(dummy.getHealth() == 5);
+	}
+
+	@Test
+	public void shouldGiveExperienceWhenKilled() {
+		Dummy dummy = new Dummy(-5, 10);
+		int actual = dummy.giveExperience();
+
+		Assert.assertTrue(actual == 10);
+
 	}
 }
