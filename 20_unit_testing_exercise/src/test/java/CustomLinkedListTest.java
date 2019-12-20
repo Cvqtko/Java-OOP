f2090abbb9b434376c1d352f3feef6287bab3f14
Cvqtko <cvqtkokirilov@gmail.com>
2019-12-20 12:31:56 +0200

    Some more Unit tests

--- 20_unit_testing_exercise/src/test/java/CustomLinkedListTest.java
+++ 20_unit_testing_exercise/src/test/java/CustomLinkedListTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.lang.reflect.Field;
@@ -19,10 +20,23 @@ public class CustomLinkedListTest {
 	}
 
 	@Test
-	public void creatingLinkedListShouldHaveZeroCount() {
+	public void creatingLinkedListShouldHaveZeroCount() throws IllegalArgumentException, IllegalAccessException {
 		Field field = Arrays.stream(CustomLinkedList.class.getDeclaredFields()).filter(f -> f.getName().equals("count"))
 				.findFirst().orElse(null);
 
 		assertNotNull(field);
+		field.setAccessible(true);
+		int count = (int) field.get(this.list);
+		assertEquals(0, count);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void getOnEmptyListShouldThrowExceptionWithZeroAsIndex() {
+		this.list.get(0);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void getOnEmptyListShouldThrowExceptionWithNegativeAsIndex() {
+		this.list.get(-1);
 	}
 }
