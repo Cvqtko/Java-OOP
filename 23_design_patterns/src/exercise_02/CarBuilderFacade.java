68867a49442f6d4ac23ab58b2789e721b264aba9
Cvqtko <cvqtkokirilov@gmail.com>
2019-12-22 17:07:01 +0200

    Design patterns exercises

--- 23_design_patterns/src/exercise_02/CarBuilderFacade.java
+++ 23_design_patterns/src/exercise_02/CarBuilderFacade.java
@@ -6,4 +6,15 @@ public class CarBuilderFacade {
 	public CarBuilderFacade() {
 		this.car = new Car();
 	}
+	
+	public Car build() {
+		return this.car;
+	}
+	public CarInfoBuilder info() {
+		return new CarInfoBuilder(this.car);
+	}
+	
+	public CarAddressBuilder build() {
+		return new CarAddressBuilder(car);
+	}
 }
--- 23_design_patterns/src/exercise_02/CarInfoBuilder.java
+++ 23_design_patterns/src/exercise_02/CarInfoBuilder.java
@@ -1,5 +1,5 @@
 package exercise_02;
 
-public class CarInfoBuilder {
-
+public class CarInfoBuilder extends CarBuilderFacade{
+	public CarInfoBuilder(Car car) {super.car = car;}
 }
